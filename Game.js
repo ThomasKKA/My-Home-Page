0e0ce523749523f1ac0fbc5e212e03bb766adce8
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-03-27 13:36:35 -0400

    Add files via upload

--- Game.js
+++ Game.js
@@ -3,25 +3,25 @@
 const questions = [
     {
         question: "Who was the first Pope of the Catholic Church?",
-        image: "",
+        image: "4c1e57a5ea983cd90559f4d30acbdde9-st peter pope.jpg",
         options: ["St. Paul", "St. Peter", "St. John", "St. Augustine"],
         answer: "St. Peter"
     },
     {
         question: "How many Sacraments are there in the Catholic Church?",
-        image: "",
+        image: "sacraments_of_the_catholic_church.png",
         options: ["5", "7", "10", "12"],
         answer: "7"
     },
     {
         question: "Which book is the first book of the Bible?",
-        image: "",
+        image: "bible-king-james-version-authorized-kjv-1611-best-bible-for-kobo.jpg",
         options: ["Exodus", "Genesis", "Matthew", "Psalms"],
         answer: "Genesis"
     },
     {
         question: "What is the Eucharist?",
-        image: "",
+        image: "illustration-of-the-seven-catholic-sacraments-e1726836985787.png",
         options: [
             "A prayer book",
             "The Body and Blood of Christ",
@@ -32,7 +32,7 @@ const questions = [
     },
     {
         question: "Who is the mother of Jesus?",
-        image: "",
+        image: "images.jpg",
         options: ["Elizabeth", "Martha", "Mary", "Ruth"],
         answer: "Mary"
     }
