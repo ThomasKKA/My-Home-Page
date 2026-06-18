e534820800a27985ee4ccd3680f7317f2ef75cdb
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-06-18 12:04:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const button = document.getElementById("myButton");
-button.addEventListener("click", function () {
-    window.open("NextPage.html", "_blank");
-    
-});
+const myButton = document.getElementById("myButton");
+if (myButton) {
+    myButton.addEventListener("click", function () {
+        window.open("NextPage.html", "_blank");
+    });
+}
 
-
-
-const goBackButton= document.getElementById("goBackButton");
-button.addEventListener("click", function () {
-    window.location.href = "index.html";
-
-});
+const goBackButton = document.getElementById("goBackButton");
+if (goBackButton) {
+    goBackButton.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+}
