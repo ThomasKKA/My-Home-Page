6e3767e5c2012102e2a5b91a9633fc80ca67165d
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-06-18 11:31:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ button.addEventListener("click", function () {
     window.open("NextPage.html", "_blank");
     
 });
+
+
+
+const button = document.getElementById("goBackButton");
+button.addEventListener("click", function () {
+    window.location.href = "index.html";
+
+});
