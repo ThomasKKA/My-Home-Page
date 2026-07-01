bf8b00c5fe4b07b4c68a6f16f26a03ae2aebb956
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-07-01 19:14:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const myButton = document.getElementById("myButton");
 if (myButton) {
     myButton.addEventListener("click", function () {
-        window.open("NextPage.html", "_blank");
+        window.location.href = "NextPage.html";
     });
 }
 
