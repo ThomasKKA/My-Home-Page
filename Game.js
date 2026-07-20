9fe38cd9cb9479d2a0a29d18e918a7f8b4ec876c
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-07-20 11:52:11 -0400

    Add files via upload

--- Game.js
+++ Game.js
@@ -36,17 +36,40 @@ const questions = [
         image: "images.jpg",
         options: ["Elizabeth", "Martha", "Mary", "Ruth"],
         answer: "Mary"
+    },
+    {
+        question: " In the stations of the cross, how many times did Jesus fall?",
+        image: "StationsOfTheCross_04_Main-640w.jpg",
+        options: ["4", "1", "3", "6"],
+        answer: "3"
     }
 ];
 
 let currentQuestion = 0;
 let score = 0;
 
+// background start game
+const startBtn = document.getElementById("start-btn");
+const quizContainer = document.getElementById("quizContainer");
+const startImage = document.getElementById("Start-Image");
+const text = document.getElementById("Background-Text");
+const reStartbtn = document.getElementById("ReStart-Button");
+
 const questionEl = document.getElementById("question");
 const optionsEl = document.getElementById("options");
 const nextBtn = document.getElementById("next-btn");
 const scoreEl = document.getElementById("score");
 const img = document.getElementById("question-image");
+
+
+/*sounds Effect*/
+const correctSound = new Audio("freesound_community-correct-choice-43861.mp3");
+const wrongSound = new Audio("gta-ctw-wrong-buzzer-sound.mp3");
+const congrats = new Audio("correcto_Xgyp04B.mp3");
+const backgroundMusic = new Audio("hauntsync-habemus-papam-catholic-pipe-organ-tribute-339173.mp3");
+backgroundMusic.volume = 0.2; // 20% volume
+const startGame = new Audio("Video Project.mp3");
+
 function loadQuestion() {
     const q = questions[currentQuestion];
     questionEl.textContent = q.question;
@@ -64,6 +87,14 @@ function loadQuestion() {
     });
 }
 
+function shuffleQuestions() {
+    for (let i = questions.length - 1; i > 0; i--) {
+        let random = Math.floor(Math.random() * (i + 1));
+
+        [questions[i], questions[random]] = [questions[random], questions[i]];
+    }
+}
+
 function selectOption(e) {
     const selected = e.target.textContent;
     const correct = questions[currentQuestion].answer;
@@ -71,8 +102,11 @@ function selectOption(e) {
     if (selected === correct) {
         score++;
         e.target.style.backgroundColor = "green";
+        correctSound.play();
     } else {
         e.target.style.backgroundColor = "red";
+        wrongSound.play();
+
         // show correct answer
         Array.from(optionsEl.children).forEach(btn => {
             if (btn.textContent === correct) btn.style.backgroundColor = "green";
@@ -83,20 +117,74 @@ function selectOption(e) {
     Array.from(optionsEl.children).forEach(btn => btn.disabled = true);
 }
 
+
+
+
+
 nextBtn.addEventListener("click", () => {
     currentQuestion++;
     if (currentQuestion < questions.length) {
         loadQuestion();
+      
     } else {
         questionEl.textContent = "Quiz Finished!";
         optionsEl.innerHTML = "";
         scoreEl.textContent = `Your Score: ${score} / ${questions.length}`;
+        let result = score / questions.length;
+        //pause the music 
+        backgroundMusic.pause();
+        backgroundMusic.currentTime = 0; 
+        if (result >= 0.5) {
+          
+            congrats.play();
+        }
+       
+
+
         nextBtn.style.display = "none";
+        reStartbtn.style.display = "block";
+        text.style.display = "none";  
     }
 });
+console.log(startBtn);
+startBtn.addEventListener("click", () => {
+    startGame.play();
+    backgroundMusic.play();
+    backgroundMusic.loop = true;
+    startBtn.style.display = "none";
+     text.style.display = "none"; 
+    startImage.style.display = "none";
+    quizContainer.style.display = "block";
+    shuffleQuestions();
+    loadQuestion();
 
-// start the game
-loadQuestion();
+});
 
 
 
+reStartbtn.addEventListener("click", () => {
+
+    // Reset quiz
+    currentQuestion = 0;
+    score = 0;
+
+    // Hide quiz
+    quizContainer.style.display = "none";
+
+   
+
+    // Show start screen
+    startBtn.style.display = "block";
+    text.style.display = "block";   
+    startImage.style.display = "block";
+    // Hide restart button
+    reStartbtn.style.display = "none";
+
+    // Reset score
+    scoreEl.textContent = "";
+
+    // Show next button again for next game
+    nextBtn.style.display = "block";
+
+});
+/*loadQuestion();*/
